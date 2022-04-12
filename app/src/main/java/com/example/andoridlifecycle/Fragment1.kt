4726bc5451fee09a483adbb6c14e10409bc13075
Joakim Fredsvik <joakim.sol.fredsvik@gmail.com>
2022-04-12 17:19:53 +0200

    Post works

--- app/src/main/java/com/example/andoridlifecycle/Fragment1.kt
+++ app/src/main/java/com/example/andoridlifecycle/Fragment1.kt
@@ -1,16 +1,36 @@
 package com.example.andoridlifecycle
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.Rect
 import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
+import android.os.Build
+import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
+import android.os.Environment
+import android.util.Base64
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.common.Priority
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.JSONObjectRequestListener
+import com.androidnetworking.interfaces.StringRequestListener
 import com.edmodo.cropper.CropImageView
+import kotlinx.android.synthetic.main.item_custom_row.*
+import kotlinx.android.synthetic.main.item_custom_row.view.*
+import org.json.JSONObject
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
 class Fragment1 : Fragment() {
 
@@ -29,7 +49,7 @@ class Fragment1 : Fragment() {
         var view = inflater.inflate(R.layout.fragment1, container, false)
 
         image = view.findViewById<CropImageView>(R.id.image)
-        image.setOnClickListener{
+        image.setOnClickListener {
 
             var i = Intent()
 
@@ -42,20 +62,31 @@ class Fragment1 : Fragment() {
         return view
     }
 
-    var startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+    var startForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
 
-        imageUri = it.data?.data.toString()
 
-        var bitmap_image = getBitmap(requireContext(), null, imageUri, ::UriToBitmap)
+            imageUri = it.data?.data.toString()
+            println("uri $imageUri")
 
-        image.layoutParams = image.layoutParams.apply {
 
-            width = bitmap_image.width
-            height = bitmap_image.height
+            var bitmap_image = getBitmap(requireContext(), null, imageUri, ::UriToBitmap)
+
+
+            // uploadImage(createFileFromBitmap(bitmap_image))
+
+            image.layoutParams = image.layoutParams.apply {
+
+                width = bitmap_image.width
+                height = bitmap_image.height
+            }
+
+            image.setImageBitmap(bitmap_image)
+            image.background = BitmapDrawable(resources, bitmap_image)
         }
 
-        image.setImageBitmap(bitmap_image)
-        image.background = BitmapDrawable(resources, bitmap_image)
-    }
+
+
+
 
 }
--- app/src/main/java/com/example/andoridlifecycle/MainActivity.kt
+++ app/src/main/java/com/example/andoridlifecycle/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.andoridlifecycle
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -9,11 +10,17 @@ import androidx.appcompat.app.AppCompatActivity
 import com.androidnetworking.AndroidNetworking
 
 import androidx.fragment.app.FragmentManager
+import com.androidnetworking.common.Priority
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.StringRequestListener
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
 
 class MainActivity : AppCompatActivity() {
 
-
+    private lateinit var url: String;
     private lateinit var fragmentManager: FragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -55,7 +62,67 @@ class MainActivity : AppCompatActivity() {
         //var rect = (fragmentManager.findFragmentByTag("Fragment1") as Fragment1).actualCropRect!!
         //var imgW = (fragmentManager.findFragmentByTag("Fragment1") as Fragment1).image.width
         //var imgH = (fragmentManager.findFragmentByTag("Fragment1") as Fragment1).image.height
+
+        var bitmap_image = getBitmap(applicationContext, null, imageUri, ::UriToBitmap)
+
+        // sending post to server.
+        uploadImage(createFileFromBitmap(bitmap_image))
+
         Log.i("fileImg", imageUri)
         Toast.makeText(this, "Added", Toast.LENGTH_SHORT).show()
+    }
+
+    // Sends file to server
+    fun uploadImage(file: File) {
+        // mock api
+        val mock = "https://infinite-river-92056.herokuapp.com/upload"
+
+        // actual api
+        val apiUrl = "http://api-edu.gtl.ai/api/v1/imagesearch/upload"
+
+        AndroidNetworking.upload(apiUrl)
+            .addMultipartFile("image", file)
+            .setTag("uploadTest")
+            .setPriority(Priority.HIGH)
+            .addMultipartParameter("key", "value")
+            .setTag("uploadTest")
+            .build()
+            .getAsString(object : StringRequestListener {
+                override fun onResponse(response: String?) {
+                    println("response: $response")
+                }
+
+                override fun onError(error: ANError) {
+                    // handle error
+                    println("upload error: ${error.errorBody}")
+                }
+            })
+    }
+
+    /*
+    * creates file.
+    * creates ByteArrayOutputStream
+    * compresses bitmap by making it JPEG, passing inn outputstream
+    * turns output stream to bytearray
+    * writes array to file
+    * returns file.
+    * */
+    fun createFileFromBitmap(bitmap: Bitmap): File {
+        // unsure correct context!!!
+        val f = File(applicationContext.filesDir,"img.png")
+
+        f.createNewFile()
+
+        //Convert bitmap to byte array
+        val bos = ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 20 /*ignored for PNG*/, bos);
+        val bitmapdata: ByteArray = bos.toByteArray();
+
+        //write the bytes in file
+        val fos = FileOutputStream(f);
+        fos.write(bitmapdata);
+        fos.flush();
+        fos.close();
+        return f;
     }
 }
