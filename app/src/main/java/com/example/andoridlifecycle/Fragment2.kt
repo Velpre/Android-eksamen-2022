b34e99dcef10cfd8eb391ca7c153663cc4f8dc7e
SigmundGjengedal <sigmund.gjengedal@gmail.com>
2022-04-12 13:54:41 +0200

    NEW VERSION

--- app/src/main/java/com/example/andoridlifecycle/Fragment2.kt
+++ app/src/main/java/com/example/andoridlifecycle/Fragment2.kt
@@ -7,17 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.androidnetworking.AndroidNetworking
-import kotlinx.coroutines.launch
 import android.util.Log
 import android.widget.Toast
-import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.androidnetworking.error.ANError
 import com.androidnetworking.interfaces.JSONArrayRequestListener
 import com.example.andoridlifecycle.adapters.ItemAdapter
-import kotlinx.coroutines.*
-import kotlinx.coroutines.Dispatchers.Main
 import org.json.JSONArray
 import org.json.JSONException
 
@@ -27,34 +23,15 @@ class Fragment2() : Fragment() {
 
     // global members
     private var listOfUrls = ArrayList<String>()
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         AndroidNetworking.initialize(context)
-
-        // Coroutines
-            lifecycleScope.launch(Dispatchers.IO) {
-                val urls = loadDataApi("https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/PNG_transparency_demonstration_1.png/640px-PNG_transparency_demonstration_1.png")
-
-                launch(Main) {
-                    delay(7000)
-                    listOfUrls = urls
-                    println(listOfUrls.toString())
-                    val itemAdapter = ItemAdapter(requireContext(), listOfUrls)
-                    val recyclerView: RecyclerView =
-                        requireView().findViewById(R.id.recycler_view_items)
-                    recyclerView.layoutManager = GridLayoutManager(context, 3)
-                    recyclerView.adapter = itemAdapter
-
-                }
-            }
+        loadDataApi("https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/PNG_transparency_demonstration_1.png/640px-PNG_transparency_demonstration_1.png")
 
     }// onCreate ends
 
     // Step 1: API CALL
-    private fun loadDataApi(url: String): ArrayList<String> {
+    private fun loadDataApi(url: String) {
         AndroidNetworking.get("http://api-edu.gtl.ai/api/v1/imagesearch/bing")
             .addQueryParameter("url", url)
             .build()
@@ -65,6 +42,12 @@ class Fragment2() : Fragment() {
                             val obj = response.getJSONObject(i)
                             val url = obj.getString("thumbnail_link")
                             listOfUrls.add(url)
+                            val itemAdapter = ItemAdapter(requireContext(), listOfUrls)
+                            val recyclerView: RecyclerView =
+                                requireView().findViewById(R.id.recycler_view_items)
+                            recyclerView.layoutManager = GridLayoutManager(context, 3)
+                            recyclerView.adapter = itemAdapter
+
                         } catch (e: JSONException) {
                             e.printStackTrace()
                             Toast.makeText(context, "Problems getting data", Toast.LENGTH_SHORT).show()
@@ -76,10 +59,9 @@ class Fragment2() : Fragment() {
                     Toast.makeText(context, "Problems getting data", Toast.LENGTH_SHORT).show()
                 }
             })
-        return listOfUrls
     }
 
-    // setting list of urs to item view
+    // setting the fragment..I THINK hehe.
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
