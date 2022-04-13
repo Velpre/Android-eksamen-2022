891e0a89f5e37286b144db081f70e9f6e817ef9a
SigmundGjengedal <sigmund.gjengedal@gmail.com>
2022-04-13 16:03:05 +0200

    spinner on glide. Does not start soon enough tho

--- app/src/main/java/com/example/andoridlifecycle/adapters/ItemAdapter.kt
+++ app/src/main/java/com/example/andoridlifecycle/adapters/ItemAdapter.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.andoridlifecycle.R
@@ -15,10 +17,10 @@ import kotlinx.android.synthetic.main.item_custom_row.view.*
 class ItemAdapter(private val context: Context, private val urls: ArrayList<String>) :
     RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
 
-    /**
-     * Inflates the custom view which is designed in xml layout file
-     */
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
         return ViewHolder(
             LayoutInflater.from(context).inflate(
                 R.layout.item_custom_row,
@@ -30,15 +32,18 @@ class ItemAdapter(private val context: Context, private val urls: ArrayList<Stri
 
     //Binder alle items i ArrayListen , som er urls, til en Imageview
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        // spinner while loading
+        val circularProgressDrawable = CircularProgressDrawable(context)
+        circularProgressDrawable.strokeWidth = 6f
+        circularProgressDrawable.centerRadius = 40f
+        circularProgressDrawable.start()
 
-        // her kommer et array av bildeadresser
-        val url = urls[position]
         // Glide med url
-
+        val url = urls[position]
         Glide.with(context)
             .load(url)
             .diskCacheStrategy(DiskCacheStrategy.ALL)
-            .placeholder(R.drawable.placeholder)
+            .placeholder(circularProgressDrawable)
             .into(holder.imageView)
     }
 
